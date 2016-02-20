a120debf6e17230d2b928015340a56da83a0cd82
migo <migo_andres@yahoo.com>
2016-02-21 00:50:12 +0800

    added comment

--- src/mc2/MC2.java
+++ src/mc2/MC2.java
@@ -3,8 +3,6 @@ public class MC2 {
     public static void main(String[] args) {
         TodoList todoList = new TodoList();
     }
-    //Initial Commit Migo
-    //Puta
-    //Sex
+    //Edit
 
 }
